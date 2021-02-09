141ae2b54a93b98f294fcfe132d8fececbc1e289
Agil <agil08saputra95@gmail.com>
2021-02-09 19:35:28 +0700

    delete penumpang penerbangan

---
+++ delete_penumpang_penerbangan.php
@@ -0,0 +1,17 @@
+<?php
+    $username = "root";
+    $password = "";
+    $server_name = "localhost";
+    $database_name = "penerbangan"; 
+    $connection = new mysqli($server_name, $username, $password, $database_name);
+    
+    $id_data = $_GET['id'];
+    $sql_delete_data = "DELETE FROM penumpang_penerbangan WHERE id=".$id_data;
+
+    if($connection->query($sql_delete_data) === TRUE){
+        header('Location: index_penumpang_penerbangan.php');
+    }
+    else{
+        echo "Data gagal dimasukkan:".$connection->error;
+    }
+?>
